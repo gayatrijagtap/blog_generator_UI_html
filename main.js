e84a5ad82ea316606e87e70c0e7d629b99a2fe59
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2019-01-06 01:07:51 +0530

    created blog generator using html user interface

---
+++ main.js
@@ -0,0 +1,72 @@
+const createPost = function() {
+  let blog = new Object();
+  let usrTheme = document.getElementById("theme").value;
+  let themes = new Object();
+  themes["travel"] = "/blog_generator/style_sheets/travelTheme.css";
+  themes["cooking"] = "/blog_generator/style_sheets/cookingTheme.css";
+  themes["animals"] = "/blog_generator/style_sheets/animalsTheme.css";
+  themes["other"] = "/blog_generator/style_sheets/generalTheme.css";
+  blog["styleSheet"] = themes[usrTheme];
+  blog["author"] = document.getElementById("author").value;
+  blog["date"] = new Date();
+  blog["title"] = document.getElementById("title").value;
+  blog["description"] = document.getElementById("desc").value;
+  blog["post"] = new Object();
+  blog.post["title"] = document.getElementById("ptitle").value;
+  blog.post["content"] = document.getElementById("pcont").value;
+  return blog;
+};
+
+const withTag = function(tag, content, cls = "") {
+  let tagWithContent = [
+    "<",
+    tag,
+    " class = ",
+    cls,
+    ">",
+    content,
+    "</",
+    tag,
+    ">"
+  ].join("");
+  return tagWithContent;
+};
+
+const generateBlog = function(blogContent) {
+  let author = withTag("h2", blogContent.author, "left_align");
+  let date = withTag("h3", blogContent.date, "left_align");
+  let blogTitle = withTag("h1", blogContent.title);
+  let blogDescription = withTag("p", blogContent.description);
+  let blogData = author + date + blogTitle + blogDescription;
+  return blogData;
+};
+
+const generatePost = function(postData) {
+  let title = withTag("h2", postData.title);
+  let content = withTag("p", postData.content);
+  return title + content;
+};
+
+const createHTMLBlog = function(blog, posts, head) {
+  let bodyContent = blog + posts;
+  let body = withTag("body", bodyContent);
+  let htmlBody = withTag("html", head + body);
+  return htmlBody;
+};
+
+const addStyleSheet = function(styleSheet) {
+  let style =
+    "<link rel='stylesheet' type='text/css' href='" + styleSheet + "'>";
+  let head = withTag("head", style);
+  return head;
+};
+
+let createBlog = function() {
+  let blogContent = createPost();
+  let blogData = generateBlog(blogContent);
+  let postData = generatePost(blogContent.post);
+  let styleSheet = addStyleSheet(blogContent.styleSheet);
+  let final = createHTMLBlog(blogData, postData, styleSheet);
+  let blog = window.open("");
+  blog.document.write(final);
+};
